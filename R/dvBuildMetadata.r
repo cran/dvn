cce1ebcf6866df22efcbd94870067f5b3e56c5b1
Thomas J. Leeper <thosjleeper@gmail.com>
2016-06-07 17:09:00 +0000

    version 0.3.5

--- R/dvBuildMetadata.r
+++ R/dvBuildMetadata.r
@@ -1,32 +1,140 @@
-dvBuildMetadata <- function(..., format='dcterms'){
-    if(!format=='dcterms')
-        stop('Only format=="dcterms" is currently supported')
-    
-    pairls <- list(...)
-    dublincore <- c(
-        "abstract","accessRights","accrualMethod","accrualPeriodicity",
-        "accrualPolicy","alternative","audience","available",
-        "bibliographicCitation","conformsTo","contributor","coverage",
-        "created","creator","date","dateAccepted","dateCopyrighted",
-        "dateSubmitted","description","educationLevel","extent","format",
-        "hasFormat","hasPart","hasVersion","identifier","instructionalMethod",
-        "isFormatOf","isPartOf","isReferencedBy","isReplacedBy","isRequiredBy",
-        "issued","isVersionOf","language","license","mediator","medium",
-        "modified","provenance","publisher","references","relation","replaces",
-        "requires","rights","rightsHolder","source","spatial","subject",
-        "tableOfContents","temporal","title","type","valid")
-    if(any(!names(pairls) %in% dublincore))
-        stop('All names of `...` parameters must be in Dublin Core')
-    if(!'title' %in% names(pairls))
-        stop('"title" is a required metadata field')
-    entry <- newXMLNode('entry', namespaceDefinitions=
-                c(  'http://www.w3.org/2005/Atom',
-                    dcterms='http://purl.org/dc/terms/'))
-    dcchild <- function(x,y)
-        dcnode <- newXMLNode(y, x, namespace='dcterms')
-    addChildren(entry, mapply(dcchild,pairls,names(pairls)))
-    entry <- paste('<?xml version="1.0" encoding="UTF-8" ?>\n',toString.XMLNode(entry),sep='')
-    class(entry) <- c(class(entry),'dvMetadata')
-    attr(entry,'formatName') <- format
-    return(entry)
+dvBuildMetadata <- function(..., format='dcterms', validate=FALSE){
+    if(format=='dcterms'){
+        pairls <- list(...)
+        dublincore <- c(
+            "abstract","accessRights","accrualMethod","accrualPeriodicity",
+            "accrualPolicy","alternative","audience","available",
+            "bibliographicCitation","conformsTo","contributor","coverage",
+            "created","creator","date","dateAccepted","dateCopyrighted",
+            "dateSubmitted","description","educationLevel","extent","format",
+            "hasFormat","hasPart","hasVersion","identifier","instructionalMethod",
+            "isFormatOf","isPartOf","isReferencedBy","isReplacedBy","isRequiredBy",
+            "issued","isVersionOf","language","license","mediator","medium",
+            "modified","provenance","publisher","references","relation","replaces",
+            "requires","rights","rightsHolder","source","spatial","subject",
+            "tableOfContents","temporal","title","type","valid")
+        if(any(!names(pairls) %in% dublincore))
+            stop('All names of `...` parameters must be in Dublin Core')
+        if(!'title' %in% names(pairls))
+            stop('"title" is a required metadata field')
+        entry <- newXMLNode('entry', namespaceDefinitions=
+                    c(  'http://www.w3.org/2005/Atom',
+                        dcterms='http://purl.org/dc/terms/'))
+        dcchild <- function(x,y)
+            dcnode <- newXMLNode(y, x, namespace='dcterms')
+        addChildren(entry, mapply(dcchild,pairls,names(pairls)))
+        entry <- paste('<?xml version="1.0" encoding="UTF-8" ?>\n',toString.XMLNode(entry),sep='')
+        if(validate) {
+            # run an XML schema validation
+            #valid <- xmlSchemaValidate('http://purl.org/dc/terms/', entry)
+        }
+        class(entry) <- c(class(entry),'dvMetadata')
+        attr(entry,'formatName') <- format
+        return(entry)
+    } else if(format=='ddi_controlcard'){
+        warning('Support for DDI control card is still experimental')
+        
+        n <- list(...)
+        if(is.null(n$filename))
+            stop('Must specify a `filename`')
+        if(is.null(n$df))
+            stop('Must specify a dataframe as `df`')
+        df <- n$df
+        vnames <- names(df)
+        if(is.null(n$varlabels))
+            varlabels <- names(df)
+        vclass <- sapply(df, class)
+        # NEED TO FORMAT vclass TO BE 'discrete' AND 'contin' ONLY
+        vclass <- ifelse(vclass=='character', 'discrete', vclass)
+        vclass <- ifelse(vclass=='factor', 'discrete', vclass)
+        vclass <- ifelse(vclass=='integer', 'discrete', vclass)
+        vclass <- ifelse(vclass=='numeric', 'contin', vclass)
+        
+        entry <- newXMLNode('codeBook', namespaceDefinitions = 'http://www.icpsr.umich.edu/DDI')
+        # fileDscr field
+        fileDscr <- newXMLNode('fileDscr', parent=entry)
+        if(!is.null(n$filename)){
+            fileTxt <- newXMLNode('fileTxt', attrs = c(ID=n$filename), parent = fileDscr)
+        } else{
+            fileTxt <- newXMLNode('fileTxt', parent=fileDscr)
+        }
+        dimensns <- newXMLNode('dimensns', parent=fileTxt)
+            caseQnty <- newXMLNode('caseQnty', dim(df)[1], parent=dimensns)
+            varQnty <- newXMLNode('varQnty', dim(df)[2], parent=dimensns)
+        # dataDscr field
+        dataDscr <- newXMLNode('dataDscr', parent=entry)
+        for(i in 1:length(df)){
+            var <- newXMLNode('var', parent = fileDscr)
+            addAttributes(var,  #ID=paste('v1',i,sep='.'),
+                                name=vnames[i],
+                                intrvl=vclass[i])
+            location <- newXMLNode('location', attrs = c(fileid = n$filename), parent=var)
+            labl <- newXMLNode('labl', varlabels[i], attrs = c(level = 'variable'), parent=var)
+            if(inherits(df[,i], 'numeric') | inherits(df[,i], 'integer')){
+                varFormat <- newXMLNode('varFormat', attrs = c(type = 'numeric'), parent=var)
+            } else if( inherits(df[,i], 'character') | inherits(df[,i], 'AsIs')){
+                varFormat <- newXMLNode('varFormat', attrs = c(type = 'character'), parent=var)
+            } else if(inherits(df[,i], 'factor')){
+                sapply(levels(df[,i]), function(i){
+                    catgry <- newXMLNode('catgry', parent = var)
+                    z <- newXMLNode('catValu', i, parent = catgry)
+                    addChildren(var, catgry)
+                })
+                #varFormat <- newXMLNode('varFormat', attrs = c(type = 'numeric'), parent=var)
+                varFormat <- newXMLNode('varFormat', attrs = c(type = 'character'), parent=var)
+            }
+            addChildren(dataDscr, var)
+        }
+        
+        entry <- paste('<?xml version="1.0" encoding="UTF-8" ?>\n',toString.XMLNode(entry),sep='')
+        if(validate) {
+            # run an XML schema validation
+            #valid <- xmlSchemaValidate('http://www.icpsr.umich.edu/DDI', entry)
+        }
+        class(entry) <- c(class(entry),'dvMetadata')
+        attr(entry,'formatName') <- 'ddi'
+        return(entry)
+    } else if(format=='spss_controlcard') {
+        warning('Support for SPSS control card is still experimental')
+        
+        # http://guides.thedata.org/book/csv-data-spss-style-control-card-0
+        
+        n <- list(...)
+        
+        entry <- ''
+        entry <- paste(entry, 'data list list(',')\n')
+        
+        # data formats
+        # A8 8 byte character string;
+        # A character string;
+        # f10.2 numeric value, 10 decimal digits, with 2 fractional digits;
+        # f8 defaults to F8.0
+        # F defaults to F.0, i.e., numeric integer value
+        # 2 defaults to F.2, i.e., numeric float value with 2 fractional digits.
+        # 
+        # DATE                            yyyy-MM-dd
+        # DATETIME                        yyyy-MM-dd HH:mm:ss
+        entry <- paste(entry, '\n.\n')
+        
+        
+        # variable formats
+        entry <- paste(entry, 'VARIABLE LABELS\n')
+        entry <- paste(entry, '\n.\n')
+        
+        
+        # value labels
+        
+        # missing values
+        
+        # number of cases
+        entry <- paste(entry, 'NUMBER of CASES', dim(n$df)[1])
+        
+        ## NEED TO STRIP COMMAS FROM ALL CHARACTER STRINGS
+        
+        class(entry) <- c(class(entry),'dvMetadata')
+        attr(entry,'formatName') <- 'spss'
+        return(entry)
+        
+    } else
+        stop('Only format=="dcterms" is fully supported')
 }
--- README.md
+++ README.md
@@ -1,21 +1,14 @@
 # R Access to the Dataverse Network #
 
-The **dvn** package for R integrates public data sharing into the reproducible research workflow. As such, **dvn** provides access to both the [Data Sharing API](http://guides.thedata.org/node/13328) and the [Data Deposit API](http://devguide.thedata.org/features/api/data-deposit/) for the [Dataverse Network](http://thedata.org/) online data repository system. The Data Sharing API enables searches of any public dataverse, returning study and file metadata. Limited support is also provided for data download, when terms of use allow direct download of public data.
+*Many dataverse servers now use Dataverse version 4.0 or greater. A separate package, [dataverse](https://github.com/IQSS/dataverse-client-r), is being developed to work with these newer installations.*
+
+The **dvn** package for R integrates public data sharing into the reproducible research workflow. As such, **dvn** provides access to both the Data Sharing API and the Data Deposit API for the [Dataverse Network](http://thedata.org/) online data repository system. The Data Sharing API enables searches of any public dataverse, returning study and file metadata. Limited support is also provided for data download, when terms of use allow direct download of public data.
 
 The Data Deposit API allows users to directly create and modify dataverse listings (studies, metadata, and files), thereby integrating data archiving directly into the R workflow with just a few simple functions. The Data Deposit API is built on the [SWORD protocol](http://en.wikipedia.org/wiki/SWORD_%28protocol%29), though not all features are fully supported by the API.
 
 **dvn** defaults to providing access to the [Harvard Dataverse Network](https://thedata.harvard.edu/dvn/), but this can be changed in each function call or globally using `options(dvn = 'https://thedata.harvard.edu/dvn/')` for any valid [Dataverse Network](http://thedata.org/book/dataverse-networks-around-world).
 
-Users interested in downloading metadata from archives other than Dataverse may be interested in Kurt Hornik's [OAIHarvester](http://cran.r-project.org/web/packages/OAIHarvester/index.html), which offers access to any web repository that is compliant with the [Open Archives Initiative](http://www.openarchives.org/) standards.
-
-## Installation ##
-
-You can find a stable release on [CRAN](http://cran.r-project.org/web/packages/dvn/index.html), or install the latest development version from GitHub using [Hadley's](http://had.co.nz/) [devtools](http://cran.r-project.org/web/packages/devtools/index.html) package:
-```
-# install.packages("devtools")
-library(devtools)
-install_github(repo = "dvn", username = "leeper")
-```
+Users interested in downloading metadata from archives other than Dataverse may be interested in Kurt Hornik's [OAIHarvester](https://cran.r-project.org/package=OAIHarvester), which offers metadata download from any web repository that is compliant with the [Open Archives Initiative](http://www.openarchives.org/) standards. Additionally, [rdryad](https://cran.r-project.org/package=rdryad) uses OAIHarvester to interface with [Dryad](http://datadryad.org/). The [rfigshare](https://cran.r-project.org/package=rfigshare) package works in a similar spirit to **dvn** with [http://figshare.com/](http://figshare.com/).
 
 ## Functions for the Data Sharing API ##
 ### Metadata Search Functions ###
@@ -40,6 +33,17 @@ The core workflow for the Data Deposit API involves creating a study listing usi
 * `dvAddFile`, to add file(s) or active R dataframes to a study
 * `dvReleaseStudy`, to publicly release a study
 
+Here is simple code to enact that workflow:
+
+```
+options(dvn.user = 'username', dvn.pwd = 'password')
+doc <- dvServiceDoc()
+meta <- dvBuildMetadata(title="My Study", creator="Doe, John")
+study <- dvCreateStudy(doc, meta)
+dvAddFile(study, filename)
+dvReleaseStudy(study)
+```
+
 The following functions allow users to view and modify existing studies:
 * `dvDeleteFile`, to delete a file from a study
 * `dvDeleteStudy`, to deaccession a released study, or delete an unreleased study
@@ -47,3 +51,25 @@ The following functions allow users to view and modify existing studies:
 * `dvStudyStatement`, to access a summary of a study (including citation and stable URI)
 * `dvServiceDoc`, to identify the dataverse(s) accessible to a user (This is not particularly relevant to users with access to only one dataverse)
 * `dvStudyAtom`, to view study-level URIs for using the Data Deposit API (This is not important for most users, unless they plan to call `dvDepositQuery` directly)
+
+
+## Installation ##
+
+[![CRAN Version](http://www.r-pkg.org/badges/version/dvn)](http://cran.r-project.org/package=dvn)
+![Downloads](http://cranlogs.r-pkg.org/badges/dvn)
+[![Build Status](https://travis-ci.org/ropensci/dvn.png?branch=master)](https://travis-ci.org/leeper/ropensci)
+
+You can find a stable release on [CRAN](http://cran.r-project.org/package=dvn), or install the latest development version from GitHub using [Hadley's](http://had.co.nz/) [devtools](http://cran.r-project.org/package=devtools) package:
+
+```R
+if (!require("devtools")) {
+    install.packages("devtools")
+    library("devtools")
+}
+install_github("ropensci/dvn")
+library("dvn")
+```
+
+---
+
+[![](http://ropensci.org/public_images/github_footer.png)](http://ropensci.org)
